390c9651d2cbc81ba5192f04363a750cc77a9da2
Sivriuk Yuri <sivriuk1999@gmail.com>
2020-01-28 18:38:39 +0200

    Add homework 9.6

---
+++ src/sivriuk/com/Task9_6.java
@@ -0,0 +1,29 @@
+package sivriuk.com;
+import java.util.Arrays;
+import java.util.Random;
+/*6*) написать метод который преобразует матрицу в одномерный массив.
+     Например если матрица была 3x4 то массив будет на 12 элементов.*/
+public class Task9_6 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[][] array = new int[3][4];
+        int[] array2 = new int[3 * 4];
+        int index = 0;
+        System.out.println("Матрица:");
+        modifyingMatrix(array, array2, index, random);
+        System.out.println("\nArray2: " + Arrays.toString(array2));
+    }
+
+    public static void modifyingMatrix(int[][] array, int[] array2, int index, Random random) {
+        for (int i = 0; i < array.length; ++i) {
+            for (int j = 0; j < array.length; ++j) {
+                array[i][j] = random.nextInt(100);
+                System.out.print(array[i][j] + " ");
+                array2[index++] = array[i][j];
+            }
+            System.out.println();
+        }
+    }
+
+}
+
